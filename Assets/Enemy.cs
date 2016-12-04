9bda049f6b19444fd2d7369311536d3a6548a820
Aayush Kapoor <xeoneux@icloud.com>
2016-12-04 09:39:28 +0530

    Update enemy movement

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -5,5 +5,15 @@ public class Enemy : MonoBehaviour
     public float speed = 10f;
 
     private Transform target;
-    private int wavepointIndex = 0;
+
+    void Start()
+    {
+        target = Waypoints.points[0];
+    }
+
+    void Update()
+    {
+        var dir = target.position - transform.position;
+        transform.Translate(dir.normalized*speed*Time.deltaTime, Space.World);
+    }
 }
