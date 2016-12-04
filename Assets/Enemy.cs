43fe73a318312581bbf30b2d725b1ab45342bdc0
Aayush Kapoor <xeoneux@icloud.com>
2016-12-04 09:49:51 +0530

    Destroy enemy on completion

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -5,6 +5,7 @@ public class Enemy : MonoBehaviour
     public float speed = 10f;
 
     private Transform target;
+    private int waypointIndex = 0;
 
     void Start()
     {
@@ -15,5 +16,22 @@ public class Enemy : MonoBehaviour
     {
         var dir = target.position - transform.position;
         transform.Translate(dir.normalized*speed*Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.5f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        if (waypointIndex >= Waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        waypointIndex++;
+        target = Waypoints.points[waypointIndex];
     }
 }
