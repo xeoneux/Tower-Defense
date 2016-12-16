e2e2e1b54af76edaebd9a773ce006b86427f1dee
Aayush Kapoor <xeoneux@icloud.com>
2016-12-16 15:04:04 +0530

    Add wave spawner

---
+++ Assets/WaveSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+
+    void Update()
+    {
+        if (countdown <= 0f)
+        {
+            SpawnWave();
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+    }
+
+    void SpawnWave()
+    {
+        Debug.Log("Wave Incoming!!!");
+    }
+}
